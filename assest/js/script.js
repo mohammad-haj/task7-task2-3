33a62a293a2447d60a1b9f15f46c1b2598498c80
mohammad-haj <mohammadhag44@gmail.com>
2024-05-22 20:49:48 +0300

    task7-task2-3

---
+++ assest/js/script.js
@@ -0,0 +1,19 @@
+
+function toggleText() {
+    var hiddenText = document.getElementById("hidden-text");
+    if (hiddenText.style.display === "none") {
+      hiddenText.style.display = "block";
+    } else {
+      hiddenText.style.display = "none";
+    }
+  }
+
+  function toggleText1() {
+    var hiddenText1 = document.getElementById("hidden-text1");
+    if (hiddenText1.style.display === "none") {
+      hiddenText1.style.display = "block";
+    } else {
+      hiddenText1.style.display = "none";
+    }
+  }
+
